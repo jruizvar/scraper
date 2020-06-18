cb5a3fe7ca65eaa76195cfe3b70503b02ac48936
Jose Cupertino Ruiz Vargas <jcruizva@hotmail.com>
2020-06-18 07:27:31 -0300

    Improve API

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
-from flask import Flask, send_from_directory
+from flask import (
+    Flask, redirect, request, send_from_directory, url_for
+)
 from raspatela import main
 
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return "Use the URL /&#60 youtuber &#62 to get the data"
-
-
-@app.route("/<youtuber>")
-def getdata(youtuber):
-    filename = main(youtuber)
-    return f"File available at the URL /{filename}"
+    if request.method == "POST":
+        youtuber = request.form["youtuber"]
+        filename = main(youtuber)
+        return redirect(url_for("download", filename=filename))
+    return """
+        <form method='post'>
+            <p><input type=text name=youtuber></p>
+            <p><input type=submit value=Download></p>
+        </form>
+    """
 
 
 @app.route("/<filename>")
